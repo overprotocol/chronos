ae278b632e065f4daddaf3bc35b1a5eb243ca032
Jay <jay@superblock.xyz>
2024-10-24 15:36:41 +0900

    fixed logic and test for WeakSubjectivityPeriod

--- beacon-chain/core/electra/transition_test.go
+++ beacon-chain/core/electra/transition_test.go
@@ -73,7 +73,7 @@ func TestProcessEpoch_CanProcessElectra(t *testing.T) {
 
 	b := st.Balances()
 	require.Equal(t, params.BeaconConfig().MaxValidatorsPerCommittee, uint64(len(b)))
-	require.Equal(t, uint64(268733096904), b[0])
+	require.Equal(t, uint64(665533096904), b[0]) // 255933096904 + 409600000000(=amountAvailForProcessing/10)
 
 	s, err := st.InactivityScores()
 	require.NoError(t, err)
