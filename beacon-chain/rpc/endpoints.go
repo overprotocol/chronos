5144a4e372d2a310ef9622d3fd8f475f9fc52cb6
Jun Song <87601811+syjn99@users.noreply.github.com>
2024-11-07 14:52:53 +0900

    Use GetBlockAttestationV2 at handler (#14624)

--- beacon-chain/rpc/endpoints.go
+++ beacon-chain/rpc/endpoints.go
@@ -532,7 +532,7 @@ func (s *Service) beaconEndpoints(
 			middleware: []middleware.Middleware{
 				middleware.AcceptHeaderHandler([]string{api.JsonMediaType}),
 			},
-			handler: server.GetBlockAttestations,
+			handler: server.GetBlockAttestationsV2,
 			methods: []string{http.MethodGet},
 		},
 		{
