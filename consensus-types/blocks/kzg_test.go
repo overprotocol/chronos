2819ebe6629ca4fc1022f8a9c39248cd16b8ade6
qqqeck <nathan@superblock.xyz>
2024-11-21 12:12:36 +0900

    Delete sidecar test

--- consensus-types/blocks/kzg_test.go
+++ consensus-types/blocks/kzg_test.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/prysmaticlabs/gohashtree"
 	fieldparams "github.com/prysmaticlabs/prysm/v5/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/v5/container/trie"
 	enginev1 "github.com/prysmaticlabs/prysm/v5/proto/engine/v1"
@@ -30,70 +29,6 @@ func Test_MerkleProofKZGCommitment_Altair(t *testing.T) {
 	require.NoError(t, err)
 	_, err = MerkleProofKZGCommitment(body, 0)
 	require.ErrorIs(t, errUnsupportedBeaconBlockBody, err)
-}
-
-func Test_MerkleProofKZGCommitment(t *testing.T) {
-	kzgs := make([][]byte, 3)
-	kzgs[0] = make([]byte, 48)
-	_, err := rand.Read(kzgs[0])
-	require.NoError(t, err)
-	kzgs[1] = make([]byte, 48)
-	_, err = rand.Read(kzgs[1])
-	require.NoError(t, err)
-	kzgs[2] = make([]byte, 48)
-	_, err = rand.Read(kzgs[2])
-	require.NoError(t, err)
-	pbBody := &ethpb.BeaconBlockBodyDeneb{
-		ExecutionPayload: &enginev1.ExecutionPayloadDeneb{
-			ParentHash:    make([]byte, fieldparams.RootLength),
-			FeeRecipient:  make([]byte, 20),
-			StateRoot:     make([]byte, fieldparams.RootLength),
-			ReceiptsRoot:  make([]byte, fieldparams.RootLength),
-			LogsBloom:     make([]byte, 256),
-			PrevRandao:    make([]byte, fieldparams.RootLength),
-			BaseFeePerGas: make([]byte, fieldparams.RootLength),
-			BlockHash:     make([]byte, fieldparams.RootLength),
-			Transactions:  make([][]byte, 0),
-			ExtraData:     make([]byte, 0),
-		},
-		BlobKzgCommitments: kzgs,
-	}
-
-	body, err := NewBeaconBlockBody(pbBody)
-	require.NoError(t, err)
-	index := 1
-	_, err = MerkleProofKZGCommitment(body, 10)
-	require.ErrorIs(t, errInvalidIndex, err)
-	proof, err := MerkleProofKZGCommitment(body, index)
-	require.NoError(t, err)
-
-	// Test the logic of topProof in MerkleProofKZGCommitment.
-	commitmentsRoot, err := getBlobKzgCommitmentsRoot(kzgs)
-	require.NoError(t, err)
-	bodyMembersRoots, err := topLevelRoots(body)
-	require.NoError(t, err, "Failed to get top level roots")
-	bodySparse, err := trie.GenerateTrieFromItems(
-		bodyMembersRoots,
-		logBodyLength,
-	)
-	require.NoError(t, err, "Failed to generate trie from member roots")
-	require.Equal(t, bodyLength, bodySparse.NumOfItems())
-	topProof, err := bodySparse.MerkleProof(kzgPosition)
-	require.NoError(t, err, "Failed to generate Merkle proof")
-	require.DeepEqual(t,
-		topProof[:len(topProof)-1],
-		proof[fieldparams.LogMaxBlobCommitments+1:],
-	)
-
-	root, err := body.HashTreeRoot()
-	require.NoError(t, err)
-	// Partially verify if the commitments root is in the body root.
-	// Proof of the commitment length is not needed.
-	require.Equal(t, true, trie.VerifyMerkleProof(root[:], commitmentsRoot[:], kzgPosition, topProof[:len(topProof)-1]))
-
-	chunk := makeChunk(kzgs[index])
-	gohashtree.HashChunks(chunk, chunk)
-	require.Equal(t, true, trie.VerifyMerkleProof(root[:], chunk[0][:], uint64(index+KZGOffset), proof))
 }
 
 // This test explains the calculation of the KZG commitment root's Merkle index
@@ -177,61 +112,4 @@ func Benchmark_MerkleProofKZGCommitment(b *testing.B) {
 		_, err := MerkleProofKZGCommitment(body, index)
 		require.NoError(b, err)
 	}
-}
-
-func Test_VerifyKZGInclusionProof(t *testing.T) {
-	kzgs := make([][]byte, 3)
-	kzgs[0] = make([]byte, 48)
-	_, err := rand.Read(kzgs[0])
-	require.NoError(t, err)
-	kzgs[1] = make([]byte, 48)
-	_, err = rand.Read(kzgs[1])
-	require.NoError(t, err)
-	kzgs[2] = make([]byte, 48)
-	_, err = rand.Read(kzgs[2])
-	require.NoError(t, err)
-	pbBody := &ethpb.BeaconBlockBodyDeneb{
-		ExecutionPayload: &enginev1.ExecutionPayloadDeneb{
-			ParentHash:    make([]byte, fieldparams.RootLength),
-			FeeRecipient:  make([]byte, 20),
-			StateRoot:     make([]byte, fieldparams.RootLength),
-			ReceiptsRoot:  make([]byte, fieldparams.RootLength),
-			LogsBloom:     make([]byte, 256),
-			PrevRandao:    make([]byte, fieldparams.RootLength),
-			BaseFeePerGas: make([]byte, fieldparams.RootLength),
-			BlockHash:     make([]byte, fieldparams.RootLength),
-			Transactions:  make([][]byte, 0),
-			ExtraData:     make([]byte, 0),
-		},
-		BlobKzgCommitments: kzgs,
-	}
-
-	body, err := NewBeaconBlockBody(pbBody)
-	require.NoError(t, err)
-	root, err := body.HashTreeRoot()
-	require.NoError(t, err)
-	index := 1
-	proof, err := MerkleProofKZGCommitment(body, index)
-	require.NoError(t, err)
-
-	header := &ethpb.BeaconBlockHeader{
-		BodyRoot:   root[:],
-		ParentRoot: make([]byte, 32),
-		StateRoot:  make([]byte, 32),
-	}
-	signedHeader := &ethpb.SignedBeaconBlockHeader{
-		Header:    header,
-		Signature: make([]byte, fieldparams.BLSSignatureLength),
-	}
-	sidecar := &ethpb.BlobSidecar{
-		Index:                    uint64(index),
-		KzgCommitment:            kzgs[index],
-		CommitmentInclusionProof: proof,
-		SignedBlockHeader:        signedHeader,
-	}
-	blob, err := NewROBlob(sidecar)
-	require.NoError(t, err)
-	require.NoError(t, VerifyKZGInclusionProof(blob))
-	proof[2] = make([]byte, 32)
-	require.ErrorIs(t, errInvalidInclusionProof, VerifyKZGInclusionProof(blob))
 }
