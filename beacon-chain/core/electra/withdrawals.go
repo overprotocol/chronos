18f8e4b5f4041ab3e127353a5d10efcc4aee3857
qqqeck <nathan@superblock.xyz>
2024-11-22 14:40:52 +0900

    Fix comments

--- beacon-chain/core/electra/withdrawals.go
+++ beacon-chain/core/electra/withdrawals.go
@@ -24,63 +24,73 @@ import (
 //
 // Spec pseudocode definition:
 //
-// def process_withdrawal_request(state: BeaconState, withdrawal_request: WithdrawalRequest) -> None:
-//     amount = withdrawal_request.amount
-//     is_full_exit_request = amount == FULL_EXIT_REQUEST_AMOUNT
-
-//     # If partial withdrawal queue is full, only full exits are processed
-//     if len(state.pending_partial_withdrawals) == PENDING_PARTIAL_WITHDRAWALS_LIMIT and not is_full_exit_request:
-//         return
-
-//     validator_pubkeys = [v.pubkey for v in state.validators]
-//     # Verify pubkey exists
-//     request_pubkey = withdrawal_request.validator_pubkey
-//     if request_pubkey not in validator_pubkeys:
-//         return
-//     index = ValidatorIndex(validator_pubkeys.index(request_pubkey))
-//     validator = state.validators[index]
-
-//     # Verify withdrawal credentials
-//     is_correct_source_address = validator.withdrawal_credentials[12:] == withdrawal_request.source_address
-//     if not is_correct_source_address:
-//         return
-//     # Verify the validator is active
-//     if not is_active_validator(validator, get_current_epoch(state)):
-//         return
-//     # Verify exit has not been initiated
-//     if validator.exit_epoch != FAR_FUTURE_EPOCH:
-//         return
-//     # Verify the validator has been active long enough
-//     if get_current_epoch(state) < validator.activation_epoch + SHARD_COMMITTEE_PERIOD:
-//         return
-
-//     pending_balance_to_withdraw = get_pending_balance_to_withdraw(state, index)
-
-//     if is_full_exit_request:
-//         # Only exit validator if it has no pending withdrawals in the queue
-//         if pending_balance_to_withdraw == 0:
-//             initiate_validator_exit(state, index)
-//         return
-
-//     has_sufficient_effective_balance = validator.effective_balance >= MIN_ACTIVATION_BALANCE
-//     has_excess_balance = state.balances[index] > MIN_ACTIVATION_BALANCE + pending_balance_to_withdraw
-
-// # Only allow partial withdrawals with compounding withdrawal credentials
-// if has_sufficient_effective_balance and has_excess_balance:
+// ProcessWithdrawalRequests processes the validator withdrawals from the provided execution payload
+// into the beacon state triggered by the execution layer.
 //
-//	to_withdraw = min(
-//	    state.balances[index] - MIN_ACTIVATION_BALANCE - pending_balance_to_withdraw,
-//	    amount,
+// Spec pseudocode definition:
+//
+// def process_withdrawal_request(
+//
+//	state: BeaconState,
+//	withdrawal_request: WithdrawalRequest
+//
+// ) -> None:
+//
+//	amount = withdrawal_request.amount
+//	is_full_exit_request = amount == FULL_EXIT_REQUEST_AMOUNT
+//
+//	# If partial withdrawal queue is full, only full exits are processed
+//	if len(state.pending_partial_withdrawals) == PENDING_PARTIAL_WITHDRAWALS_LIMIT and not is_full_exit_request:
+//	    return
+//
+//	validator_pubkeys = [v.pubkey for v in state.validators]
+//	# Verify pubkey exists
+//	request_pubkey = withdrawal_request.validator_pubkey
+//	if request_pubkey not in validator_pubkeys:
+//	    return
+//	index = ValidatorIndex(validator_pubkeys.index(request_pubkey))
+//	validator = state.validators[index]
+//
+//	# Verify withdrawal credentials
+//	has_correct_credential = has_execution_withdrawal_credential(validator)
+//	is_correct_source_address = (
+//	    validator.withdrawal_credentials[12:] == withdrawal_request.source_address
 //	)
-//	exit_queue_epoch = compute_exit_epoch_and_update_churn(state, to_withdraw)
-//	withdrawable_epoch = Epoch(exit_queue_epoch + MIN_VALIDATOR_WITHDRAWABILITY_DELAY)
-//	state.pending_partial_withdrawals.append(
-//	    PendingPartialWithdrawal(
+//	if not is_correct_source_address:
+//	    return
+//	# Verify the validator is active
+//	if not is_active_validator(validator, get_current_epoch(state)):
+//	    return
+//	# Verify exit has not been initiated
+//	if validator.exit_epoch != FAR_FUTURE_EPOCH:
+//	    return
+//	# Verify the validator has been active long enough
+//	if get_current_epoch(state) < validator.activation_epoch + SHARD_COMMITTEE_PERIOD:
+//	    return
+//
+//	pending_balance_to_withdraw = get_pending_balance_to_withdraw(state, index)
+//
+//	if is_full_exit_request:
+//	    # Only exit validator if it has no pending withdrawals in the queue
+//	    if pending_balance_to_withdraw == 0:
+//	        initiate_validator_exit(state, index)
+//	    return
+//
+//	has_sufficient_effective_balance = validator.effective_balance >= MIN_ACTIVATION_BALANCE
+//	has_excess_balance = state.balances[index] > MIN_ACTIVATION_BALANCE + pending_balance_to_withdraw
+//
+//	if has_sufficient_effective_balance and has_excess_balance:
+//	    to_withdraw = min(
+//	        state.balances[index] - MIN_ACTIVATION_BALANCE - pending_balance_to_withdraw,
+//	        amount
+//	    )
+//	    exit_queue_epoch = compute_exit_epoch_and_update_churn(state, to_withdraw)
+//	    withdrawable_epoch = Epoch(exit_queue_epoch + MIN_VALIDATOR_WITHDRAWABILITY_DELAY)
+//	    state.pending_partial_withdrawals.append(PendingPartialWithdrawal(
 //	        index=index,
 //	        amount=to_withdraw,
 //	        withdrawable_epoch=withdrawable_epoch,
-//	    )
-//	)
+//	    ))
 func ProcessWithdrawalRequests(ctx context.Context, st state.BeaconState, wrs []*enginev1.WithdrawalRequest) (state.BeaconState, error) {
 	ctx, span := trace.StartSpan(ctx, "electra.ProcessWithdrawalRequests")
 	defer span.End()
