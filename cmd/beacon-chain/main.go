d3d4e2918c45af519d82d51acc2bd1d8b5a7e66a
Jun Song <syjn99@gmail.com>
2024-12-03 13:08:49 -0600

    Remove interop genesis service from beacon node (#14417)
    * Remove interop dependencies from production binary for beacon-chain. Specifically, remove the interop genesis service.
    
    Finding links to pebble:
    bazel query 'somepath(//cmd/beacon-chain, @com_github_cockroachdb_pebble//...)' --notool_deps
    
    * Update INTEROP.md
    
    * Remove interop config
    
    * Remove ancient interop script
    
    * Add electra support for premine genesis
    
    * Add example of --chain-config-file, test interop instructions
    
    * Fixes
    
    * Add binary size reduction
    
    * Update binary size reduction
    
    * Fix duplicate switch case
    
    * Move CHANGELOG entries to unreleased section
    
    * gofmt
    
    * fix
    
    ---
    
    Co-authored-by: Preston Van Loon <pvanloon@offchainlabs.com>

--- cmd/beacon-chain/main.go
+++ cmd/beacon-chain/main.go
@@ -61,8 +61,6 @@ var appFlags = []cli.Flag{
 	flags.BlobBatchLimit,
 	flags.BlobBatchLimitBurstFactor,
 	flags.InteropMockEth1DataVotesFlag,
-	flags.InteropNumValidatorsFlag,
-	flags.InteropGenesisTimeFlag,
 	flags.SlotsPerArchivedPoint,
 	flags.EnableOverNodeRPCEndpoints,
 	flags.DisableDebugRPCEndpoints,
--- config/features/deprecated_flags.go
+++ config/features/deprecated_flags.go
@@ -83,6 +83,16 @@ var (
 		Usage:  deprecatedUsage,
 		Hidden: true,
 	}
+	deprecatedInteropGenesisTimeFlag = &cli.Uint64Flag{
+		Name:   "interop-genesis-time",
+		Usage:  deprecatedUsage,
+		Hidden: true,
+	}
+	deprecatedInteropNumValidatorsFlag = &cli.Uint64Flag{
+		Name:   "interop-num-validators",
+		Usage:  deprecatedUsage,
+		Hidden: true,
+	}
 )
 
 // Deprecated flags for both the beacon node and validator client.
@@ -101,8 +111,11 @@ var deprecatedFlags = []cli.Flag{
 	deprecatedDisableGRPCGateway,
 	deprecatedEnableExperimentalState,
 	deprecatedEnableCommitteeAwarePacking,
+	deprecatedInteropGenesisTimeFlag,
 }
 
 // deprecatedBeaconFlags contains flags that are still used by other components
 // and therefore cannot be added to deprecatedFlags
-var deprecatedBeaconFlags []cli.Flag
+var deprecatedBeaconFlags = []cli.Flag{
+	deprecatedInteropNumValidatorsFlag,
+}
