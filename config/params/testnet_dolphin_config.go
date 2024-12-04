63674814e711b6b097c30f4af4ca5b8a4660cf71
Jay <jay@superblock.xyz>
2024-12-04 13:59:44 +0900

    updated config values for dolphin testnet

--- config/params/testnet_dolphin_config.go
+++ config/params/testnet_dolphin_config.go
@@ -1,15 +1,11 @@
 package params
 
-import (
-	"math"
-)
-
 // UseDolphinNetworkConfig uses the Dolphin beacon chain specific network config.
 func UseDolphinNetworkConfig() {
 	cfg := BeaconNetworkConfig().Copy()
 	cfg.BootstrapNodes = []string{
 		// Dolphin testnet boot nodes
-		"enr:-LG4QGaXDTDc5_-AvUXuWxoYlT2Ce9dSlLi4Kx0Wzv7PFBSFWqRubay-w-IY5lay30YpEbP6_yNQtXa1QcrRD1PSdYqGAZFLTRaKh2F0dG5ldHOIAAAAAAAAAACCaWSCdjSCaXCEgMdLF4RvdmVykNBNsU8AAAAY__________-Jc2VjcDI1NmsxoQOr1euFU8IZdyGo8jbIzJD0Z8VcRnt9xrIF-aOrRvQjPYN1ZHCCyyA",
+		"enr:-LG4QIKxU43azrDAd777KngiPXD2Vd1_TlIFDq33rDZ95mNuFxcO-HbetclOxx0Ofdgmr6PXC5jnglaYMLyhRcQ0ariGAZOP_nHOh2F0dG5ldHOIAAAAAAAAAACCaWSCdjSCaXCEp6xM9IRvdmVykNBNsU8AAAAY__________-Jc2VjcDI1NmsxoQOM3AK4-wojli0G8baFWHRRPMNREBTUefi7ltQJXTpRZ4N1ZHCCyyA",
 	}
 	OverrideBeaconNetworkConfig(cfg)
 }
@@ -17,7 +13,7 @@ func UseDolphinNetworkConfig() {
 // DolphinConfig defines the config for the Dolphin beacon chain testnet.
 func DolphinConfig() *BeaconChainConfig {
 	cfg := MainnetConfig().Copy()
-	cfg.GenesisValidatorsRoot = [32]byte{72, 53, 56, 66, 146, 92, 179, 239, 84, 134, 155, 20, 196, 84, 186, 245, 125, 16, 110, 201, 247, 155, 198, 125, 119, 186, 120, 204, 89, 247, 6, 37}
+	cfg.GenesisValidatorsRoot = [32]byte{197, 106, 18, 116, 72, 45, 248, 71, 14, 95, 225, 167, 251, 5, 217, 99, 189, 20, 169, 20, 11, 236, 169, 89, 239, 170, 171, 48, 115, 61, 69, 71}
 	cfg.ConfigName = DolphinName
 	cfg.GenesisForkVersion = []byte{0x0, 0x00, 0x00, 0x28}
 	cfg.DepositChainID = 541764
@@ -26,14 +22,12 @@ func DolphinConfig() *BeaconChainConfig {
 	cfg.AltairForkVersion = []byte{0x1, 0x00, 0x00, 0x28}
 	cfg.BellatrixForkEpoch = 0
 	cfg.BellatrixForkVersion = []byte{0x2, 0x00, 0x00, 0x28}
-	cfg.CapellaForkEpoch = 10
+	cfg.CapellaForkEpoch = 0
 	cfg.CapellaForkVersion = []byte{0x3, 0x00, 0x00, 0x28}
-	cfg.DenebForkEpoch = math.MaxUint64
+	cfg.DenebForkEpoch = 0
 	cfg.DenebForkVersion = []byte{0x4, 0x00, 0x00, 0x28}
-	cfg.AlpacaForkEpoch = math.MaxUint64
+	cfg.AlpacaForkEpoch = 0
 	cfg.AlpacaForkVersion = []byte{0x5, 0x00, 0x00, 0x28}
-	cfg.IssuanceRate = [11]uint64{20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 0}
-	cfg.MaxRewardAdjustmentFactors = [11]uint64{1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000}
 	cfg.InitializeForkSchedule()
 	cfg.InitializeDolphinDepositPlan()
 	cfg.InitializeInactivityValues()
