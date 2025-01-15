e93754d4c469e6476297d025d6b65281d78d0a4f
syjn99 <syjn99@gmail.com>
2025-01-15 11:32:35 +0900

    Add type alias: BeaconStateBadger

---
+++ proto/prysm/v1alpha1/badger.go
@@ -0,0 +1,9 @@
+// This file exports the consensus containers for the Badger hard fork.
+// As the Badger hard fork introduces no changes to the containers, they remain identical to those in the Electra (Alpaca) hard fork.
+// If any changes to the containers are required, the *.proto files should be updated accordingly.
+
+package eth
+
+// BeaconState
+
+type BeaconStateBadger = BeaconStateElectra
